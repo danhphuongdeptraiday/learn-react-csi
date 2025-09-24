02174e272b9a6fddda09fcaaa7581f018d718148
danhphuongdeptraiday <nguyendanhphuong0305@gmail.com>
2025-09-24 21:03:13 +0700

    feat: update script login homework for Ha Anh

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Login.css"; // import css file
 
 function Login() {
+  const [usernameInput, setUsernameInput] = React.useState("");
+  const [passwordInput, setPasswordInput] = React.useState("");
+  const [checkUsername, setCheckUsername] = React.useState(false);
+  const [checkPassword, setCheckPassword] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+
+  const myAccount = {
+    username: "hanhanhdao",
+    password: "toibiham123",
+  };
+
+  const handleChangeUsernameInput = (e) => {
+    setUsernameInput(e.target.value);
+    if (e.target.value === myAccount.username) {
+      setCheckUsername(true);
+    } else {
+      setCheckUsername(false);
+    }
+  };
+
+  const handleChangePasswordInput = (e) => {
+    setPasswordInput(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault(); // web k bi reload
+  };
+
+  // option + shift + F
+
   return (
     <div className="login-container">
       <form className="login-form">
@@ -12,6 +42,8 @@ function Login() {
             type="username"
             name="username"
             placeholder="Enter username"
+            value={usernameInput}
+            onChange={handleChangeUsernameInput}
           />
         </div>
 
@@ -21,11 +53,16 @@ function Login() {
             type="password"
             name="password"
             placeholder="Enter password"
+            value={passwordInput}
+            onChange={handleChangePasswordInput}
           />
         </div>
+
+        <button onClick={handleLogin}>Login</button>
+        <p>{message}</p>
       </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
